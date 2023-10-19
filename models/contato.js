58aba46e28a3bebdfb98c9cf45c31c9e0190f805
pedrobits <pedroksk2@gmail.com>
2023-10-19 15:56:40 -0300

    feat: :pencil2: Modelo de Contato

---
+++ models/contato.js
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+
+const contatoSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      sparse: true,
+      trim: true,
+      lowercase: true,
+    },
+    mobile: {
+      type: String,
+      unique: true,
+      sparse: true,
+    },
+    Tel1: {
+      type: String,
+      unique: true,
+      sparse: true,
+    },
+    address: {
+      street: String,
+      city: String,
+      state: String,
+      zip: String,
+    },
+    birthday: Date,
+    notes: String,
+    socialMedia: {
+      facebook: String,
+      instagram: String,
+      twitter: String,
+      linkedin: String,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Contato = mongoose.model("Contatos", contatoSchema);
+
+export default Contato;
